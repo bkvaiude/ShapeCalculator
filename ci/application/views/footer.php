bdbe065866755b54648616defd370e5e1aca31ec
Bhushan Kishor Vaiude <bkvaiude@gmail.com>
2012-12-19 07:43:16 +0530

    First Commit

---
+++ ci/application/views/footer.php
@@ -0,0 +1,30 @@
+
+
+      <hr>
+
+      <div class="footer">
+        <p style="float:left;">Bhushan Kishor Vaiude </p><p style="float:right;">Email: bkvaiude@gmail.com</p>
+      </div>
+
+    </div> <!-- /container -->
+
+    <!-- Le javascript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="<?php echo base_url() ?>media/vendors/assets/js/jquery.js"></script>
+    <script src="<?php echo base_url() ?>media/vendors/assets/js/bootstrap-transition.js"></script>
+    <script src="<?php echo base_url() ?>media/vendors/assets/js/bootstrap-alert.js"></script>
+    <script src="<?php echo base_url() ?>media/vendors/assets/js/bootstrap-modal.js"></script>
+    <script src="<?php echo base_url() ?>media/vendors/assets/js/bootstrap-dropdown.js"></script>
+    <script src="<?php echo base_url() ?>media/vendors/assets/js/bootstrap-scrollspy.js"></script>
+    <script src="<?php echo base_url() ?>media/vendors/assets/js/bootstrap-tab.js"></script>
+    <script src="<?php echo base_url() ?>media/vendors/assets/js/bootstrap-tooltip.js"></script>
+    <script src="<?php echo base_url() ?>media/vendors/assets/js/bootstrap-popover.js"></script>
+    <script src="<?php echo base_url() ?>media/vendors/assets/js/bootstrap-button.js"></script>
+    <script src="<?php echo base_url() ?>media/vendors/assets/js/bootstrap-collapse.js"></script>
+    <script src="<?php echo base_url() ?>media/vendors/assets/js/bootstrap-carousel.js"></script>
+    <script src="<?php echo base_url() ?>media/vendors/assets/js/bootstrap-typeahead.js"></script>
+	<script src="<?php echo base_url() ?>media/vendors/assets/js/alertify.min.js"></script>
+	<script src="<?php echo base_url() ?>media/app/js/scripts.js"></script>
+  </body>
+</html>
---
+++ ci/application/views/header.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Shape Calculator</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Shape Calculator">
+    <meta name="author" content="Bhushan Kishor Vaiude">
+
+    <!-- Le styles -->
+    <link href="<?php echo base_url() ?>media/vendors/assets/css/bootstrap.css" rel="stylesheet">
+    <style type="text/css">
+      body {
+        padding-top: 20px;
+        padding-bottom: 60px;
+      }
+
+      /* Custom container */
+      .container {
+        margin: 0 auto;
+        max-width: 1000px;
+      }
+      .container > hr {
+        margin: 60px 0;
+      }
+
+
+      /* Supporting marketing content */
+      .marketing {
+        margin: 60px 0;
+      }
+      .marketing p + h4 {
+        margin-top: 28px;
+      }
+
+
+    </style>
+    <link href="<?php echo base_url() ?>media/vendors/assets/css/bootstrap-responsive.css" rel="stylesheet">
+
+    <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+
+    <!-- Fav and touch icons -->
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?php echo base_url() ?>media/vendors/assets/ico/apple-touch-icon-144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?php echo base_url() ?>media/vendors/assets/ico/apple-touch-icon-114-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?php echo base_url() ?>media/vendors/assets/ico/apple-touch-icon-72-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" href="<?php echo base_url() ?>media/vendors/assets/ico/apple-touch-icon-57-precomposed.png">
+	<link rel="shortcut icon" href="<?php echo base_url() ?>media/vendors/assets/ico/favicon.png">
+	<!-- include the core styles -->
+	<link rel="stylesheet" href="<?php echo base_url() ?>media/vendors/assets/css/alertify.core.css" />
+	<!-- include a theme, can be included into the core instead of 2 separate files -->
+	<link rel="stylesheet" href="<?php echo base_url() ?>media/vendors/assets/css/alertify.default.css" />  
+  </head>
+
+  <body>
+
+    <div class="container">
+	  <div class="page-header">
+        <h3 style="background-image: url('<?php echo base_url() ?>media/app/images/header_bg.gif');background-repeat: no-repeat; height:60px;"> &nbsp;Shape Calculator</h3>
+      </div>
+      <!-- Example row of columns -->
+      <div class="row-fluid">
+        <div class="span6">
+          <h2>Welcome to Shape Calculator</h2>
+          <p>Shape Calculator is an interactive web application. To the right you will
+find the 3 step application. Follow the instructions in each step.
+Clicking cancel will take you back to step 1. Enjoy!
+A small river named Duden flows by their place and supplies it with the
+necessary regelialia. It is a paradisematic country, in which roasted parts of
+sentences fly into your mouth.
+Even the all-powerful Pointing has no control about the blind texts it is an
+almost unorthographic life One day however a small line of blind text by the
+name of Lorem Ipsum decided to leave for the far World of Grammar. The
+Big Oxmox advised her not to do so, because there were thousands of bad
+Commas. </p>
+        </div>
